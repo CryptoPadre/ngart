b4f89319c6f912309598db32bcdbda978f68f4da
Tamas Gavlider <tamasgavlider@gmail.com>
2025-05-20 14:55:51 +0200

    move the animations from gallery to home page/add google fonts/animation to text on home page

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,21 +1,5 @@
-import styles from "../styles/Gallery.module.css";
-import Whale from "../assets/whale.png";
-import Panther from "../assets/panther.png";
-
-export default function Gallery() {
-  return (
-    <>
-      <div className={styles.Bg}>
-        <div id="wrapper">
-          <div className={styles.foreground}>
-            <img src={Whale} alt="whale" />
-          </div>
-
-          <div className={styles.background}>
-            <img alt="panther" src={Panther} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+export default function Gallery(){
+    return (
+        <h1>Gallery</h1>
+    )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,42 @@
-import styles from '../styles/Home.module.css'
-import { Row, Container, Col } from 'react-bootstrap';
+import { Row, Container, Col } from "react-bootstrap";
+import styles from "../styles/Home.module.css";
+import Whale from "../assets/whale.png";
+import Panther from "../assets/panther.png";
 
-export default function Home(){
-    return (
-        <Container>
+export default function Home() {
+  return (
+    <>
+      <Container>
         <Row>
-          <Col className='text-center'>
-          <h1 className={styles.Name}>Niki Gavlider</h1></Col>
+          <Col className="text-center">
+            <h1 className={styles.Name}>Niki Gavlider</h1>
+          </Col>
         </Row>
-        <div className={`${styles.Intro} d-flex flex-column flex-md-row text-center`}>
-          <Col ><h1>Artist</h1></Col>
-          <Col><span>by</span></Col>
-          <Col > <h2>Heart</h2></Col>
-          <Col> <span>&</span></Col>
-          <Col > <h3>Soul</h3></Col>
-        </div>
+
+        <Col className={styles.Word1}>
+          <h1>Artist</h1>
+          <span>by</span>
+        </Col>
+        <Col className={styles.Word2}>
+        
+          <h2>Heart</h2>
+       
+          <span>&</span>
+       
+          <h2>Soul</h2>
+        </Col>
       </Container>
-    );
+      <div className={styles.Bg}>
+        <div id="wrapper">
+          <div className={styles.foreground}>
+            <img src={Whale} alt="whale" />
+          </div>
+
+          <div className={styles.background}>
+            <img alt="panther" src={Panther} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
