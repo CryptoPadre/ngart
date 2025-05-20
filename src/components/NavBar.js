d82d72007569520804fbb035ba08a0793c6c19b4
Tamas Gavlider <tamasgavlider@gmail.com>
2025-05-20 13:51:34 +0200

    change href to Navlink in Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,37 @@
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import styles from '../styles/NavBar.module.css'
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
-export default function NavBar(){
-    return (
-        <>
-          {[false, 'sm'].map((expand) => (
-            <Navbar key={expand} expand={expand} className={`${styles.NavBar } bg-body-tertiary mb-3`} fixed="top">
-              <Container fluid>
-                <Navbar.Brand href="/" className={styles.NavLink}>NGArt</Navbar.Brand>
-                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                <Navbar.Offcanvas
-                  id={`offcanvasNavbar-expand-${expand}`}
-                  aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                  placement="end"
-                >
-                  <Offcanvas.Body>
-                    <Nav className="justify-content-end flex-grow-1 pe-3">
-                      <Nav.Link href="/"><i class="fa-solid fa-house"></i></Nav.Link>
-                      <Nav.Link href="/gallery"><i class="fa-solid fa-palette"></i></Nav.Link>
-                      <Nav.Link href="/about"><i class="fa-solid fa-address-card"></i></Nav.Link>
-                      <Nav.Link href="/contact" ><i class="fa-solid fa-envelope"></i></Nav.Link>
-                    </Nav>
-                  </Offcanvas.Body>
-                </Navbar.Offcanvas>
-              </Container>
-            </Navbar>
-          ))}
-        </>
-      );
+export default function NavBar() {
+  return (
+    <>
+      {[false, 'sm'].map((expand) => (
+        <Navbar key={expand} expand={expand} className={`${styles.NavBar} bg-body-tertiary mb-3`} fixed="top">
+          <Container fluid>
+            <Navbar.Brand as={NavLink} to="/" className={styles.NavLink}>
+              NGArt
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link as={NavLink} to="/" end><i className="fa-solid fa-house"></i></Nav.Link>
+                  <Nav.Link as={NavLink} to="/gallery"><i className="fa-solid fa-palette"></i></Nav.Link>
+                  <Nav.Link as={NavLink} to="/about"><i className="fa-solid fa-address-card"></i></Nav.Link>
+                  <Nav.Link as={NavLink} to="/contact"><i className="fa-solid fa-envelope"></i></Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
 }
