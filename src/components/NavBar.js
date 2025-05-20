6305cfc8f9d1c1c26363986af2c36efc509cd010
Tamas Gavlider <tamasgavlider@gmail.com>
2025-05-20 18:28:16 +0200

    custom hook to navbar to disappear the nav on small screen if clicked outside the menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,41 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import styles from '../styles/NavBar.module.css';
-import { NavLink } from 'react-router-dom';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle'
 
 export default function NavBar() {
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
   return (
     <>
-      {[false, 'sm'].map((expand) => (
-        <Navbar key={expand} expand={expand} className={`${styles.NavBar} bg-body-tertiary mb-3`} fixed="top">
-          <Container fluid>
-            <Navbar.Brand as={NavLink} to="/" className={styles.NavLink}>
-              NGArt
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link as={NavLink} to="/" end><i className="fa-solid fa-house"></i></Nav.Link>
-                  <Nav.Link as={NavLink} to="/gallery"><i className="fa-solid fa-palette"></i></Nav.Link>
-                  <Nav.Link as={NavLink} to="/about"><i className="fa-solid fa-address-card"></i></Nav.Link>
-                  <Nav.Link as={NavLink} to="/contact"><i className="fa-solid fa-envelope"></i></Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+      <Navbar expand="md"  expanded={expanded} className={`bg-body-tertiary ${styles.NavBar}` }fixed="top">
+        <Container fluid>
+          <Navbar.Brand as={NavLink} to="/" className={styles.NavLink}>
+            NGArt
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" ref={ref}
+          onClick={() => setExpanded(!expanded)}/>
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={NavLink} to="/" end>
+                  <i className="fa-solid fa-house"></i>
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/gallery">
+                  <i className="fa-solid fa-palette"></i>
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/about">
+                  <i className="fa-solid fa-address-card"></i>
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/contact">
+                  <i className="fa-solid fa-envelope"></i>
+                </Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
