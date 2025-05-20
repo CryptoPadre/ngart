36c4d95c2cbe630df5e33e25c350edbde4362052
Tamas Gavlider <tamasgavlider@gmail.com>
2025-05-21 00:02:40 +0200

    create the basic structure of gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,27 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Frida from '../assets/frida.png';
+import styles from '../styles/Gallery.module.css'
+
 export default function Gallery(){
     return (
-        <h1>Gallery</h1>
+        <Container>
+      <Row >
+        <Col xs={8} md={6} lg={6}>
+          <Image  src={Frida} thumbnail />
+        </Col>
+        <Col xs={8} md={6} lg={6}>
+          <Image  src={Frida} thumbnail />
+        </Col>
+        <Col xs={8} md={6} lg={6}>
+          <Image  src={Frida} thumbnail />
+        </Col>
+        <Col xs={8} md={6} lg={6}>
+          <Image  src={Frida} thumbnail />
+        </Col>
+      </Row>
+    </Container>
     )
 }
