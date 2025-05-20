c694233e82f083fa812fdc5d57551ca94496bc30
Tamas Gavlider <tamasgavlider@gmail.com>
2025-05-20 19:48:20 +0200

    add flex to text on home page/ made it constant on mobile devices

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,37 +6,13 @@ import Panther from "../assets/panther.png";
 export default function Home() {
   return (
     <>
-      <Container>
-        <Row>
-          <Col className="text-center">
-            <h1 className={styles.Name}>Niki Gavlider</h1>
-          </Col>
-        </Row>
+      <div className={styles.TextBlock}>
+        <h1 className={`text-center ${styles.Name}`}>Niki Gavlider</h1>
 
-        <Col className={styles.Word1}>
-          <h1>Artist</h1>
-          <span>by</span>
-        </Col>
-        <Col className={styles.Word2}>
-        
-          <h2>Heart</h2>
-       
-          <span>&</span>
-       
-          <h2>Soul</h2>
-        </Col>
-      </Container>
-      <div className={styles.Bg}>
-        <div id="wrapper">
-          {/*
-          <div className={styles.foreground}>
-            <img src={Whale} alt="whale" />
-          </div>
-   */}
-          <div className={styles.background}>
-            <img alt="panther" src={Panther} />
-          </div>
-        </div>
+        <p className={`text-center ${styles.Word}`}>Artist by Heart & Soul</p>
+      </div>
+      <div className={styles.background}>
+        <img alt="panther" src={Panther} />
       </div>
     </>
   );
