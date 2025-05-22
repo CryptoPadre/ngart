5baec96942505a6917de99b9d8120fa7a9e48b75
Tamas Gavlider <tamasgavlider@gmail.com>
2025-05-22 22:04:59 +0200

    add spinner while hte home page img is not loaded

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 import Panther from "../assets/panther.png";
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
   return (
     <>
     <div className={styles.Wrapper}>
@@ -11,7 +19,12 @@ export default function Home() {
         <p className={`text-center ${styles.Word}`}>Artist by Heart & Soul</p>
       </div>
       <div className={styles.background}>
-        <img alt="panther" src={Panther} />
+      <img
+            alt="panther"
+            src={Panther}
+            onLoad={handleImageLoad}
+            style={loading ? { display: <Spinner animation="grow"/> } : {}}
+          />
       </div>
       </div>
     </>
